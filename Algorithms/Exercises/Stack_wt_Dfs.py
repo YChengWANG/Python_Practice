cdb1fca894e6dcc237d2aa76a01644febc95ba5d
YChengWANG <ucdcheng@ucdavis.edu>
2021-03-01 23:39:21 +0800

    master

--- Algorithms/Exercises/Stack_wt_Dfs.py
+++ Algorithms/Exercises/Stack_wt_Dfs.py
@@ -1,3 +1,4 @@
+
 maze = [[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
         [1,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,1],
         [1,0,1,0,1,1,1,1,1,1,1,0,1,0,1,0,1],
@@ -24,5 +25,27 @@ dirs = [
 ]
 
 def Maze_dfs(x1,y1,x2,y2):
-    CurNode = Stack()
+    path = []
+    path.append((x1,y1))
+    while len(path)>0:
+        CurNode = path[-1]
+        #print(path)
+        if CurNode[0] == x2 and CurNode[1] == y2:
+            for p in path:
+                print(p,end='->')
+            print('End')
+            return True
+        for dir in dirs:
+            NextNode = dir(CurNode[0],CurNode[1])
+            if maze[NextNode[0]][NextNode[1]] == 0:
+                path.append(NextNode)
+                maze[NextNode[0]][NextNode[1]] = 2
+                break
+        else:
+            maze[NextNode[0]][NextNode[1]] = 2
+            path.pop()
+    else:
+        print("There is No one Path")
+        return False
 
+Maze_dfs(1,1,5,13)
