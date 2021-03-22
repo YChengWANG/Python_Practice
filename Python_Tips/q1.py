1e02e5234153be81a8c3fa80064bfe0e41eb8ac5
YChengWANG <ucdcheng@ucdavis.edu>
2021-03-22 22:31:44 +0800

    master

---
+++ Python_Tips/q1.py
@@ -0,0 +1,17 @@
+s = list(map(str,input().split()))
+print()
+
+# i = 0
+# n = len(a)
+# res = 0abcdefghijklmnopqrstuvwxyz 
+
+
+# while i<n:
+#     for word in s:
+#         print(s)
+#         res += 1
+#         if word == a[i]:
+#             i += 1
+#             res -= 1
+            
+# print(res)        
